674aaa5945809a86151f97683ec335642b569ce0
CaelOConnor <coconnor10@students.stonehill.edu>
2025-01-09 18:18:35 -0500

    re-made teh ui with the dropdown in the box still need to work on javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Watched video "How To Create To-Do List App Using HTML CSS And JavaScript | Task App In JavaScript" by GreatStack
 
-const inputBox = document.getElementById("input-box");
+/*const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 const dropDownBtn = document.querySelector(".dropdown__button");
 const dropDownMenu = document.querySelector(".dropdown__menu");
@@ -44,4 +44,31 @@ showWorkouts();
 dropDownBtn.addEventListener('click', () => {
     dropDownMenu.classList.toggle("hide");
 });
+*/
 
+//add new wokrout day with dropwdown for the exercises
+function addNewDay() {
+    const newDayInput = document.getElementById("newDay");
+    const newDay = newDayInput.value.trim();
+    const daysSelect = document.getElementById("days");
+
+    if(inputBox.value === ''){
+        alert("You must write a name for the day!");
+        return;
+    }
+    const option = document.createElement("option");
+    option.textContent = newDay;
+    
+    daysSelect.appendChild(option);
+    newDayInput.value = "";
+    saveData();
+}
+
+function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML);
+}
+
+function showWorkouts(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showWorkouts();
